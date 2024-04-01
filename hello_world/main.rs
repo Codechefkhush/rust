06217dc0510708c8848895667a91f4ffa24132d7
Khushal Nagmote <khushalnagmote4828@gmail.com>
2024-04-01 22:17:15 +0530

    feat:add code

---
+++ hello_world/main.rs
@@ -0,0 +1,9 @@
+//Code to print "Hello World, Khushal!" to console
+
+fn main () {
+    println!("Hello World, Khushal!");
+}
+
+
+//println! is calling rust macros, if you want to call function use it without exclamation mark
+
