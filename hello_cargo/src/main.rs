9cf6dcf5253b4e4358730d457258c7377e571559
Khushal Nagmote <125303323+Codechefkhush@users.noreply.github.com>
2024-04-01 22:23:26 +0530

    feat:add code

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+//Code to print "Hello, world!" to console
+
+
+fn main() {
+    println!("Hello, world!");
+}
+
+//Cargo build - to generate executable file
+//Cargo run - to generate executable file and run program
+//Cargo check - to compile code without executable
