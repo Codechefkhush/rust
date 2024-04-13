a13551739b4a6bf09fd29df167167b4d2b23ba84
Khushal Nagmote <khushalnagmote4828@gmail.com>
2024-04-13 23:58:55 +0530

    feat:update struct code

--- structure/src/main.rs
+++ structure/src/main.rs
@@ -15,10 +15,32 @@ fn main() {
         contact: 123456789,
     };
 
-    user1.username= String::from("Diyu05");
+    //  user1.username= String::from("Diyu05");
 
     println!("{}", user1.contact);
     println!("{}", user1.email);
     println!("{}", user1.username);
     println!("{}", user1.userid);
+
+    //Using struct update syntax
+    let mut user2 = User {
+        email: String::from("Diyu05@gmail.com"),
+        ..user1
+    };
+
+    println!("{}", user2.contact);
+    println!("{}", user2.email);
+    println!("{}", user2.username);
+    println!("{}", user2.userid);
 }
+
+//Returning User instance with function implementation
+// fn build_user (userid: u64, username: String, email: String, contact: u64 ) -> User {
+//     User {
+//         userid, //field init shorthand property
+//         active: true,
+//         username,
+//         email,
+//         contact,
+//     };
+// }
